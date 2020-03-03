46db96e1a9982b297c3af70d2106394b9e972a1c
Luke Duncan <luke.will.duncan@gmail.com>
2020-03-02 22:06:30 -0600

    Adding in signin/signup logic, Implementing JWT auth to validate Book CRUD

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,13 +1,21 @@
 var router = require('express').Router();
+var mongoose = require('mongoose');
+var passport = require('passport');
+var config = require('../config/database');
+require('../config/passport')(passport);
+var express = require('express');
+var jwt = require('jsonwebtoken');
+var User = require("../models/user");
+var Book = require("../models/book");
 
 // GET api/books
 router.get('/', passport.authenticate('jwt', { session: false }), function (req, res) {
-  res.json("test")
+  res.json("view all books created")
 });
 
 // POST api/books
-router.post('/', function (req, res) {
-  res.json("test")
+router.get('/', passport.authenticate('jwt', { session: false }), function (req, res) {
+  res.json("create a book")
 });
 
 module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -10,12 +10,38 @@ var Book = require("../models/book");
 
 // api/signup - register to get account
 router.post("/signup", function (req, res) {
-  
+  if (!req.body.username || !req.body.password) {
+    res.json({ success: false, msg: 'Please include both username and password in your request.' });
+  } else {
+    var newUser = new User({
+      username: req.body.username,
+      password: req.body.password
+    });
+    // save the user
+    newUser.save(function (err) {
+      if (err) {
+        return res.json({ success: false, msg: 'Username is invalid. Please select another username.' });
+      }
+      res.json({ success: true, msg: 'Your account has been created!' });
+    });
+  }
 });
 
 // api/signin - signin to receive JWT for future requests
 router.post('/signin', function (req, res) {
-  
+  User.findOne({
+    username: req.body.username
+  }, function (err, user) {
+    if (err) {
+      throw err;
+    }
+
+    if (!user) {
+      res.status(401).send({ success: false, msg: 'Signin failed.' });
+    } else {
+      // Return JWT
+    }
+  });
 });
 
 // Book Routes
